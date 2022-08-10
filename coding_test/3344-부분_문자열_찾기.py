97bd49c0b92cfd23efac454661c71e051c500f67
cyeongy <cyeongy@gmail.com>
2022-08-10 16:33:22 +0900

    Add: Data Analysis lecture files

---
+++ coding_test/3344-부분_문자열_찾기.py
@@ -0,0 +1,22 @@
+import re
+
+query = 'tawerqccbc'
+target = 'abtc'
+
+A = query
+B = target
+
+answer = "NO"
+
+idx = 0
+
+for a in A:
+    print(a, B[idx])
+
+    if B[idx] == a:
+        idx += 1
+    if idx >= len(B):
+        answer = "YES"
+
+
+print(answer)
