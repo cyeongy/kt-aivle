3ed2dcbd821b7c0fa4e2d6db9392e855b6d96775
cyeongy <cyeongy@gmail.com>
2022-08-16 13:35:09 +0900

    Add: mini project #01 / seoul bus station data

--- coding_test/3349-구조 요원.py
+++ coding_test/3349-구조 요원.py
@@ -1,8 +1,5 @@
 import sys
 
-for i in range(1):
-    print(i)
-
 N = int(sys.stdin.readline())
 x, y = map(int, sys.stdin.readline().split())
 
@@ -13,5 +10,5 @@ x *= sign
 sub = []
 
 for a in range(x + 1):
-    temp = ((N ** 2) + (a ** 2)) / 100 + ((x - a) ** 2 + (y ** 2)) / 25
-    print(temp)
+    temp = ((N ** 2) + (a ** 2)) + (((x - a) ** 2) + (y ** 2)) * 4
+    print(a, temp)
