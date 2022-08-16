e3cd04ea2a6a447bd90da26cc3fdc099821367f8
cyeongy <cyeongy@gmail.com>
2022-08-16 10:57:14 +0900

    Add: coding test practice

---
+++ coding_test/3349-구조 요원.py
@@ -0,0 +1,17 @@
+import sys
+
+for i in range(1):
+    print(i)
+
+N = int(sys.stdin.readline())
+x, y = map(int, sys.stdin.readline().split())
+
+sign = 1 if x >= 0 else -1
+
+x *= sign
+
+sub = []
+
+for a in range(x + 1):
+    temp = ((N ** 2) + (a ** 2)) / 100 + ((x - a) ** 2 + (y ** 2)) / 25
+    print(temp)
