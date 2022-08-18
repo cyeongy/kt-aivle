b29a051e1f8f823543dcfa2673d6dccc3b9dc18a
cyeongy <cyeongy@gmail.com>
2022-08-18 16:33:36 +0900

    Update: mini project#01

--- coding_test/test.py
+++ coding_test/test.py
@@ -1,39 +1,13 @@
-import sys
-from decimal import Decimal
-from fractions import Fraction
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-a = '0.31984'
+# 가능한 font list 확인
+import matplotlib.font_manager as fm
+f = [f.name for f in fm.fontManager.ttflist]
+print(f)
+print(mpl.get_configdir())
 
-print(a[2:])
-print(len(a))
+# 확인 이후
+plt.rc('font', family='Malgun Gothic')
 
-#
-# temp = [1, 3, 5, 7]
-#
-# print(temp.index(3))
-#
-# colors = {
-#     'R': (1, 0, 0),
-#     'G': (0, 1, 0),
-#     'B': (0, 0, 1),
-#     (1, 0, 0): 'R',
-#     (0, 1, 0): 'G',
-#     (0, 0, 1): 'B',
-#     (1, 1, 0): 'Y',
-#     (0, 1, 1): 'C',
-#     (1, 0, 1): 'M',
-# }
-#
-#
-# def add(ref1, ref2):
-#     return tuple(1 if lhs + rhs > 0 else 0 for lhs, rhs in zip(ref1, ref2))
-#
-#
-# N, M = map(int, sys.stdin.readline().split())
-#
-# arr = [[(1, 1, 0)] * M for _ in range(N)]
-#
-# arr[0][0] = add(arr[0][0], (0, 2, 0))
-#
-# print(arr)
-#
+fm.findSystemFonts()
