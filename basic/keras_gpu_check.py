c6493db50a67affa6408064524d7ae595b0102fd
cyeongy <cyeongy@gmail.com>
2022-10-19 06:43:25 +0900

    Update: gitignore **/add_data

--- basic/keras_gpu_check.py
+++ basic/keras_gpu_check.py
@@ -1,3 +1,8 @@
 from tensorflow.python.client import device_lib
+from tensorflow import keras
+import tensorflow as tf
+import keras
 
-print(device_lib.list_local_devices())
+print(tf.config.list_physical_devices('CPU'))
+print(tf.config.list_physical_devices('GPU'))
+# print(device_lib.list_local_devices())
