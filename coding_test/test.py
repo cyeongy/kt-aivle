8b57c8d1a2f94956c412474844424df1d319d6c5
cyeongy <cyeongy@gmail.com>
2022-10-23 08:23:26 +0900

    Add: algorithm back 2448

--- coding_test/test.py
+++ coding_test/test.py
@@ -1,22 +1,51 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+import sys
+import copy
 
-x = np.linspace(0, 1, 50)
-p1 = np.power(x, 2)
-p2 = np.power(1-x, 2)
+# x = np.linspace(0, 1, 50)
+# p1 = np.power(x, 2)
+# p2 = np.power(1-x, 2)
+#
+# plt.plot(x, 1-p1-p2)
+# plt.xlabel('P_i')
+# plt.ylabel('Gini Index')
+# plt.grid()
+# plt.xlim(0, 1)
+# plt.xticks([0.1 * i for i in range(11)])
+#
+# plt.axvline(.5, color='red')
+# plt.axvline(.48, color='green')
+# plt.axvline(.176, color='purple')
+# plt.show()
 
-plt.plot(x, 1-p1-p2)
-plt.xlabel('P_i')
-plt.ylabel('Gini Index')
-plt.grid()
-plt.xlim(0, 1)
-plt.xticks([0.1 * i for i in range(11)])
+N = int(sys.stdin.readline())
 
-plt.axvline(.5, color='red')
-plt.axvline(.48, color='green')
-plt.axvline(.176, color='purple')
-plt.show()
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
+bandwidth = {0: [0, 1], 1: [0, 1, 2], 2: [1, 2]}
 
+Max = [[0, 0, 0], [0, 0, 0]]
+Min = [[0, 0, 0], [0, 0, 0]]
 
+for i in range(3):
+    Max[0][i] = arr[0][i]
+    Min[0][i] = arr[0][i]
+
+Max[0] = copy.deepcopy(arr[0])
+Min[0] = copy.deepcopy(arr[0])
+
+for depth in range(1, N):
+    for i in range(3):
+        Max[1][i] = arr[depth][i]
+        Min[1][i] = arr[depth][i]
+    for idx in range(3):
+        Max[1][idx] += max(list(Max[0][b] for b in bandwidth[idx]))
+        Min[1][idx] += min(list(Min[0][b] for b in bandwidth[idx]))
+        pass
+    for i in range(3):
+        Max[0][i] = Max[1][i]
+        Min[0][i] = Min[1][i]
+
+print(max(Max[1]), min(Min[1]))
